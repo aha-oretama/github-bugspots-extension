2f8344aa19ac6b0655be214e853c3f1cb9e524a1
aha-oretama <sekine_y_529@msn.com>
2018-07-09 16:13:25 +0900

    feat: Add link, regex, token in popup.

--- src/origin/popup.js
+++ src/origin/popup.js
@@ -1,18 +1,38 @@
+'use strict';
 /**
  * @author aha-oretama
  * @Date 2018/05/26.
  */
-const token = document.getElementById('token');
-const saveToken = document.getElementById('saveToken');
+const token = document.querySelector('#token');
+const regex = document.querySelector('#regex');
+const save = document.querySelector('#save');
 
-chrome.storage.sync.get('token', function (data) {
-  token.innerHTML = data.token;
+chrome.storage.sync.get('token', function(data) {
+  console.log('token: ' + data.token);
+  token.value = data.token;
 });
 
-saveToken.onclick = function (element) {
+chrome.storage.sync.get('regex', function(data) {
+  console.log('regex: ' + data.regex);
+  regex.value = data.regex || '/\b(fix(es|ed)?|close(s|d)?)\b/i'
+});
+
+document.querySelector('#supplement').addEventListener('click', function () {
+  chrome.tabs.create({ url: 'https://github.com/settings/tokens' });
+});
+
+document.querySelector('#github-bugspots-log').addEventListener('click', function () {
+  chrome.tabs.create({ url: 'https://github.com/aha-oretama/github-bugspots-extension' });
+});
+
+save.addEventListener('click', function () {
   const tokenStr = token.value;
+  const regexStr = regex.value;
   chrome.storage.sync.set({token: tokenStr}, function () {
-    console.log('token: ' + tokenStr);
+    console.log('Save token. :' + tokenStr);
+    chrome.storage.sync.set({regex: regexStr}, function () {
+      console.log('Save regex. :' + regexStr);
+      window.close()
+    });
   });
-  window.close();
-};
+});
