ded5995789527cf33770509182f57970003f87d2
aha-oretama <sekine_y_529@msn.com>
2018-07-09 18:55:41 +0900

    feat: Exec bugspots, and add score. Add teardown process.

--- src/bundle/content.js
+++ src/bundle/content.js
@@ -4,37 +4,103 @@
  */
 import Bugspots from 'github-bugspots';
 
-const commitBar = document.getElementsByClassName('commit-tease')[0];
+/**
+ * 画面要素、URLなどからパラメータを取得し、github-bugspotsを実行する。
+ * @param callback 実行後のデータへの実行関数
+ * @returns {*}
+ */
+function exeBugspots(callback) {
+  const branch = document.querySelector('.branch-select-menu > button > span').innerHTML;
+  
+  const parseUrl = (tabs) => {
+    const url = tabs[0].url;
+    const parts = url.split('/'); // https://github.com/aha-oretama/github-bugspots-extension
+    const organization = parts[3];
+    const repository = parts[4];
+    return {organization, repository}
+  };
+  
+  return chrome.storage.sync.get(['token','regex'], function(data) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      parse = parseUrl(tabs);
+      return new Bugspots(parse.organization, parse.repository, data.token).analyze(branch, data.regex)
+        .then(callback)
+        .catch(e => {
+          console.log(e);
+        });
+    });
+  });
+}
 
+function storeBugspotsData(bugspots) {
+  return chrome.storage.local.set({'bugspots': bugspots}, function (data) {
+    console.log('bugspots: ' + data.bugspots);
+  })
+}
+
+function turnOn() {
+  return exeBugspots(storeBugspotsData);
+}
+
+function turnOff() {
+  return chrome.storage.local.remove('bugspots');
+}
+
+function addScore() {
+  let fileNames = Array.from(document.querySelectorAll('td.content > span > a'), it => it.innerHTML);
+  let ageSpans = document.querySelector('td.age > span').innerHTML;
+
+  // 該当ページでなければスキップ
+  if(!ageSpans) {
+    return;
+  }
+  
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    const url = tabs[0].url;
+    chrome.storage.local.get('bugspots', function (data) {
+      for(let spot of data.bugspots.spots) {
+        for (let i = 0; i < fileNames.length; i++) {
+          
+          // The fileNames inclue only file's name , not include path. Therefore, concat url and fileNames.
+          if((`${url}/${fileNames[i]}`).endsWith(spot.file)) {
+            let score = document.createElement('a');
+            score.className = 'score';
+            score.innerHTML(`${spot.score}`);
+            ageSpans[i].appendChild();
+          }
+        }
+      }
+    })
+  });
+}
+
+function removeScore() {
+  let score = document.querySelector('a.score');
+  if(!score) {
+    score.remove();
+  }
+}
+
+const commitBar = document.getElementsByClassName('commit-tease')[0];
 let div = document.createElement('div');
 div.className = 'github-bugspots-controller';
 let button = document.createElement('button');
 button.className = 'btn btn-sm notDisplayed';
 button.innerHTML = 'bugspots';
+
 button.addEventListener('click', function (event) {
   let button = event.target;
   if (button.classList.contains('notDisplayed')) {
     button.classList.remove('notDisplayed');
     button.classList.add('displayed');
-    chrome.storage.sync.get('token', function (data) {
-      new Bugspots('aha-oretama', "TypoFixer", data.token).analyze('*', 'master')
-        .then(data => {
-          const spots = data.spots;
-          let parentSpan = document.querySelector(`a[title="${spots.file}"]`) // href tag in which file exists
-              .closest('tr.js-navigation-item') // tr in which file exists
-              .querySelector('td.age span'); // age's td which is same row at above file and to be replaced
-          let score = document.createElement('a');
-          score.className = 'score';
-          score.innerHTML(`${spots.score}`);
-          parentSpan.appendChild(score);
-        })
-    });
+    turnOn();
+    addScore()
   } else {
-    let score = document.querySelector('a.score');
-    score.remove();
-
     button.classList.remove('displayed');
     button.classList.add('notDisplayed')
+  
+    turnOff();
+    removeScore()
   }
 });
 
